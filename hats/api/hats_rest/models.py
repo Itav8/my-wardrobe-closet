ac742fdb82561e92eedf6cefd9c10a24d2f570ae
Italiz <italizv@gmail.com>
2023-04-19 17:02:03 -0400

    Setup inital hat models and endpoints

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
+
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=200)
+    style = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    location = models.ForeignKey(
+        LocationVO, related_name="locations", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.style
--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+
+# from django.shortcuts import render
+
+from common.json import ModelEncoder
+from .models import Hat, LocationVO
+from django.views.decorators.http import require_http_methods
+
 
 # Create your views here.
+class LocationVOEncoder(ModelEncoder):
+    model = LocationVO
+    properties = ["name", "import_href"]
+
+
+class HatDetailEncoder(ModelEncoder):
+    model = Hat
+    properties = ["fabric", "style", "color", "location"]
+
+
+class HatListEncoder(ModelEncoder):
+    model = Hat
+    properties = ["style"]
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_hats(request):
+    if request.method == "GET":
+        hats = Hat.objects.all()
+        return JsonResponse({"hats": hats}, encoder=HatListEncoder)
+    else:
+        content = json.loads(request.body)
+
+        try:
+            location = LocationVO.objects.get(id=content["location"])
+            content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse({"message": "Invalid location id"}, status=400)
+
+        hat = Hat.objects.create(**content)
+        return JsonResponse(hat, encoder=HatDetailEncoder)
