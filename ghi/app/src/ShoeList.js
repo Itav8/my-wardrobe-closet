b69465bfc7ae08879912078e6c35a03263d49d02
Jonathan <jonathan.r.blazer@gmail.com>
2023-04-20 16:23:11 -0400

    Delete now re-renders list automatically

--- ghi/app/src/ShoeList.js
+++ ghi/app/src/ShoeList.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 
-const ShoeList = ({ shoes }) => {
+const ShoeList = ({ dummy }) => {
     const API_URL = "http://localhost:8080/api/shoes"      // for DELETE
-
+    const [shoes, setShoes] = useState([]);
 
     const apiRequest = async (url = '', optionsObj = null, errMsg = null) => {
         try {
@@ -16,8 +16,8 @@ const ShoeList = ({ shoes }) => {
     }
 
     const handleDelete = async (id) => {
-        // const listItems = items.filter((item) => item.id !== id);
-        // setItems(listItems);
+        const newShoes = shoes.filter((shoe) => shoe.id !== id);
+        setShoes(newShoes);
 
         const deleteOptions = { method: 'DELETE' };
         const reqUrl = `${API_URL}/${id}`;
@@ -28,6 +28,20 @@ const ShoeList = ({ shoes }) => {
             console.log(`else! ${result}`)
         }
     }
+
+    const fetchData = async () => {
+        const shoesResponse = await fetch (API_URL);
+        if(shoesResponse.ok) {
+            const data = await shoesResponse.json();
+            setShoes(data.shoes);
+        } else {
+            throw new Error('Shoes Response not ok!');
+        }
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
+
 
     return (
     <div>
@@ -57,6 +71,6 @@ const ShoeList = ({ shoes }) => {
         </table>
     </div>
     )
-    }
+}
 
 export default ShoeList
