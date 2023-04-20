14a72364b856ba248318dfc34821915e7b6ca8a1
Italiz <italizv@gmail.com>
2023-04-20 13:49:03 -0400

    Added hats api endpoint fetch

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
-import Nav from './Nav';
-import ShoeList from './ShoeList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
+import Nav from "./Nav";
+import ShoeList from "./ShoeList";
+import HatList from "./HatsList";
 
 function App(props) {
-  if (props.shoes === undefined) {
+  if (props.shoes === undefined && props.hats === undefined) {
     return null;
   }
+
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/shoes" element={<ShoeList
-            shoes={props.shoes}/>} />
+          <Route path="/shoes" element={<ShoeList shoes={props.shoes} />} />
+          <Route path="/hats" element={<HatList hats={props.hats} />} />
         </Routes>
       </div>
     </BrowserRouter>
